1e1b540ec53eb368c2d61eebe835794a9353eb3e
Anurag Singh <anurags9693@gmail.com>
2025-01-30 11:01:51 +0500

    add-loading-animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,40 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import JoinOurCommunity from "./components/JoinOurCommunity";
 import LandingPage from "./components/LandingPage";
 import Team from "./components/Team";
 
 const Page = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000); 
+
+    return () => clearTimeout(timer); 
+  }, []);
 
   return (
     <div>
-      <LandingPage />
-      <Team />
-      <JoinOurCommunity />
+      {loading ? (
+        <div className="fixed inset-0 flex items-center justify-center bg-black z-50">
+          <video
+            src="./loading.mp4" // Place your video in the public folder
+            autoPlay
+            muted
+            playsInline
+            className="w-full h-full object-cover"
+          />
+        </div>
+      ) : (
+        
+        <>
+          <LandingPage />
+          <Team />
+          <JoinOurCommunity />
+        </>
+      )}
     </div>
   );
 };
