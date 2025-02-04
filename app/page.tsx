2d9b11bd01efcf810a49cc674fceb55dadbe8500
Shivam Bhosle <shivambhosle270903@gmail.com>
2025-02-04 20:26:50 +0530

    Added loader

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,32 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import JoinOurCommunity from "./components/JoinOurCommunity";
 import LandingPage from "./components/LandingPage";
 import Team from "./components/Team";
+import Loader from "./components/Loading"
 
 const Page = () => {
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 2500));
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
   return (
     <div>
-      <LandingPage />
-      <Team />
-      <JoinOurCommunity />
+      {loading ? (
+        <Loader />
+      ) : ( 
+        <>
+          <LandingPage />
+          <Team />
+          <JoinOurCommunity />
+        </>
+      )}
     </div>
   );
 };
